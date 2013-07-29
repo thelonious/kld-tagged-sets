fd59bf2db9bfa4a86b8da776f07eb000d9c41a09
Kevin Lindsey <kevin@pixate.com>
2013-07-29 13:30:29 -0500

    Add example showing how to use TaggedSet

---
+++ example/taggedSet.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var TaggedSet = require('kld-collections').TaggedSet;
+var set = new TaggedSet();
+
+// add items for tag 'a'
+set.add('one', 'a');
+set.add('two', 'a');
+set.add('three', 'a');
+
+// add items for tag 'b'
+set.add('two', 'b');
+set.add('three', 'b');
+set.add('four', 'b');
+
+// show items in 'a' only
+console.log("A Only");
+console.log("======");
+console.log(set.filter(/^a$/));
+console.log();
+
+// show items in 'b' only
+console.log("B Only");
+console.log("======");
+console.log(set.filter(/^b$/));
+console.log();
+
+// show items in 'a' and 'b'
+console.log("A and B");
+console.log("======");
+console.log(set.filter(/^ab$/));
+console.log();
