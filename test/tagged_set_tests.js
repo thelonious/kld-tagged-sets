86c60a4f64e221e914c434b3413831f9a64567b0
Kevin Lindsey <kevin@pixate.com>
2013-07-29 16:35:10 -0500

    Add more functionality to TaggedSet

--- test/tagged_set_tests.js
+++ test/tagged_set_tests.js
@@ -1,5 +1,66 @@
 var TaggedSet = require('../lib/TaggedSet');
 
+exports.add = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+
+    assert.eql(['one'], set.items());
+};
+
+exports.remove = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('one', 'b');
+    set.remove('one');
+
+    assert.eql([], set.items());
+};
+
+exports.getTags = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('one', 'b');
+
+    assert.equal('ab', set.getTags('one'));
+};
+
+exports.setTags = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('one', 'b');
+    set.setTags('one', 'cd');
+
+    assert.equal('cd', set.getTags('one'));
+};
+
+exports.prune = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('three', 'a');
+    set.add('two', 'b');
+    set.add('three', 'b');
+    set.prune(/^a$/);
+
+    assert.eql(['three', 'two'], set.items().sort());
+};
+
+exports.negatedPrune = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('three', 'a');
+    set.add('two', 'b');
+    set.add('three', 'b');
+    set.prune(/^a+.*$/, true);
+
+    assert.eql(['one', 'three'], set.items().sort());
+};
+
 exports.inFirstOnly = function(beforeExit, assert) {
     var set = new TaggedSet();
 
