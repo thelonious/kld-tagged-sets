6d43a9284fcb52f6f3e82a9bcad568c439c9fe17
Kevin Lindsey <kevin@pixate.com>
2013-07-29 12:37:57 -0500

    Be a little more careful about TaggedSet inputs

--- test/tagged_set_tests.js
+++ test/tagged_set_tests.js
@@ -62,3 +62,76 @@ exports.duplicateInSecond = function(beforeExit, assert) {
 
     assert.eql(['two'], result);
 };
+
+exports.undefinedItem = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add(undefined, 'a');
+
+    var result = set.filter(/.*/);
+
+    assert.eql([], result);
+};
+
+exports.nullItem = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add(null, 'a');
+
+    var result = set.filter(/.*/);
+
+    assert.eql([], result);
+};
+
+exports.undefinedTag = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', undefined);
+
+    var result = set.filter(/.*/);
+
+    assert.eql([], result);
+};
+
+exports.nullTag = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', null);
+
+    var result = set.filter(/.*/);
+
+    assert.eql([], result);
+};
+
+exports.undefinedPattern = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('one', 'b');
+
+    var result = set.filter(undefined);
+
+    assert.eql([], result);
+};
+
+exports.nullPattern = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('one', 'b');
+
+    var result = set.filter(null);
+
+    assert.eql([], result);
+};
+
+exports.nonRegexPattern = function(beforeExit, assert) {
+    var set = new TaggedSet();
+
+    set.add('one', 'a');
+    set.add('one', 'b');
+
+    var result = set.filter('a');
+
+    assert.eql([], result);
+};
