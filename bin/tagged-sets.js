26697f389ae256238e0a558034c18eb6a73c2813
Kevin Lindsey <kevin@pixate.com>
2013-07-29 13:29:36 -0500

    Moved tagged-sets script into its own bin directory

--- bin/tagged-sets.js
+++ bin/tagged-sets.js
@@ -2,7 +2,7 @@
 
 var fs        = require('fs'),
     async     = require('async'),
-    TaggedSet = require('./lib/TaggedSet');
+    TaggedSet = require('../lib/TaggedSet');
 
 var set = new TaggedSet();
 
